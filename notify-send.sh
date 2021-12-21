bd17cf88afc1130c1b0fa615eb94566e033bc1b4
TouchOdeath <5149109+TouchOdeath@users.noreply.github.com>
2021-12-21 09:21:46 -0600

    Create notify-send.sh

---
+++ notify-send.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yad --timeout 1 --skip-taskbar --center --no-buttons --text="" --width=200 --text-align=center --undecorated --text="<span color=\"#ffffff\" bgcolor=\"#000000\" font_size=\"large\" font_weight=\"heavy\">       $1      </span>"
